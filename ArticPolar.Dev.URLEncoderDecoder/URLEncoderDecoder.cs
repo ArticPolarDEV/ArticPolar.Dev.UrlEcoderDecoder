c98422bbb037f6f849e6d5a350de7d726e99404e
JoseLucas1303 <74744598+JoseLucas1303@users.noreply.github.com>
2023-06-19 20:30:37 -0300

    Added Source Code

---
+++ ArticPolar.Dev.URLEncoderDecoder/URLEncoderDecoder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArticPolar.Dev
+{
+    public class URLEncoderDecoder
+    {
+        public static string EncodeText(string text)
+        {
+            string encodedText = Uri.EscapeDataString(text);
+            return encodedText;
+        }
+
+        public static string DecodeText(string encodedText)
+        {
+            string decodedText = Uri.UnescapeDataString(encodedText);
+            return decodedText;
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArticPolar.Dev;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mainText = "Hello !$World&*<>";
+            string encodedText = URLEncoderDecoder.EncodeText(mainText);
+            Console.WriteLine("Encoded Text: " + encodedText);
+            string decodedText = URLEncoderDecoder.DecodeText(encodedText);
+            Console.WriteLine("Decoded Text: " + decodedText);
+            Console.ReadLine();
+        }
+    }
+}
